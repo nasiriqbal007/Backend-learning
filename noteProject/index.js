378a90134c1249f86a612117d6034ddd92197066
Nasir Iqbal <nasiriqbal1198@gmail.com>
2025-01-23 17:52:13 +0500

    improved code structure

--- noteProject/index.js
+++ noteProject/index.js
@@ -7,6 +7,7 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
   fs.readdir(`./files`, (err, files) => {
     if (files) {
