43c00b58627c7aa46c88d05550a691d55254838a
Nasir Iqbal <nasiriqbal1198@gmail.com>
2025-01-28 18:40:22 +0500

    Updated app.js with CRUD routes, MongoDB integration, and EJS rendering

--- crud_mongodb_ejs/app.js
+++ crud_mongodb_ejs/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path")
-
+const usermodel = require("./models/user")
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
@@ -11,10 +11,45 @@ app.get("/", (req, res) => {
     res.render("index")
 })
 
-app.get("/read", (req, res) => {
-    res.render("read")
+app.get("/read", async (req, res) => {
+    let users = await usermodel.find();
+    res.render("read", { users })
 })
-app.post("/create", (req, res) => {
-    res.render("read")
-})
+app.post("/create", async (req, res) => {
+    const { name, email, image } = req.body;
+    let user = await usermodel.create({
+        name,
+        email,
+        image,
+    });
+    res.redirect("/read")
+
+});
+app.get("/update/:id", async (req, res) => {
+
+    let user = await usermodel.findOne({ _id: req.params.id });
+    res.render("update", { user })
+
+});
+app.post("/update/:id", async (req, res) => {
+    const { name, email, image } = req.body;
+    let user = await usermodel.findOneAndUpdate({ _id: req.params.id }, {
+        name,
+        email,
+        image,
+
+    }, {
+        new: true
+    });
+    res.redirect("/read",)
+
+});
+
+
+app.get("/delete/:id", async (req, res) => {
+    let user = await usermodel.findByIdAndDelete(req.params.id);
+    res.redirect("/read");
+});
+
+
 app.listen(3000)
